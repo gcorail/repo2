3f5128de3fb0ff23edaee079d51c2f54768edabd
gcorail <guillermo.corail@gmail.com>
2024-01-31 16:57:46 -0300

    modificacion3

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const getProducts = require("./GetProducts");
 const getProductsCart = require("./GetProductsCart");
-const addProductCart = require("./addProductCart");
+const addProductCart = require("./AddProductCart");
 const putProduct = require("./PutProduct");
 const deleteProduct = require("./DeleteProduct");
 const getProductById = require("./GetProductById")
